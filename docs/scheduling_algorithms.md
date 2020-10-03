1ced853e19279e9ab2e37b5c4ad5ef7b3d0df1d0
Dotan Di Castro <dotan.dicastro@gmail.com>
2020-10-03 20:55:49 +0300

    scheduling single machine with due time was started

--- docs/scheduling_algorithms.md
+++ docs/scheduling_algorithms.md
@@ -2,3 +2,11 @@
 
 Here we enlist scheduling algorithms and references, when needed.
 
+## Scheduling Jobs with Deadline on a single machine
+
+### Definition of the Problem
+Taken from ["The Design of Approximation Algorithms"](https://www.amazon.com/Design-Approximation-Algorithms-David-Williamson/dp/0521195276/) by  Williamson & Shmoys, pp. 36.
+
+![](imgs/scheduling_with_deadlines_with_single_machine_definition.png)
+
+
---
+++ src/scheduling/single_machine_with_deadlines.py
@@ -0,0 +1,18 @@
+from src.utils.random_utils import process_random
+
+
+class SingleMachineWithDeadline:
+
+    def __init__(self,
+                 maximal_num_jobs,
+                 rnd=0,
+                 method=""):
+        self.random = process_random(rnd)
+        self.num_jobs= self.random.randint(maximal_num_jobs)
+        self.release = []
+        self.lengths = []
+        self.due_time = []
+        for _ in range(self.num_jobs):
+            self.release.append(self.random.uniform())
+            self.lengths.append(self.random.uniform())
+            self.due_time.append(self.random.uniform())
